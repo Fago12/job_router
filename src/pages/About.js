3b446348cf9b3807ce89ce061b84a7a6d7015483
Fago12 <ayoyemisola@gmail.com>
2023-08-03 11:20:27 +0100

    August 3

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className='about'>
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div className='home'>
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestias
+        delectus ipsum expedita. Cupiditate sequi magnam fugiat aperiam quidem
+        autem ipsa repellat delectus aut voluptas, praesentium in similique
+        ipsum quaerat.
+      </p>
+    </div>
+  );
+}
